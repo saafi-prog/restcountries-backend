88ac091dfd74da240317345d19652adfa826a378
manal benallal <manelbenallal@hotmail.fr>
2022-04-14 16:36:47 +0200

    creat first web service

--- restcountries-backend-api/src/main/java/co/simplon/p25/api/controller/restCountriesController.java
+++ restcountries-backend-api/src/main/java/co/simplon/p25/api/controller/restCountriesController.java
@@ -5,16 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.simplon.p25.api.services.restCountriesService;
+
 @RestController
 @RequestMapping ("/restCountries")
 public class restCountriesController{
 	private final restCountriesService service;
 	
-	public restCountriesController(restCountries service){
-		this.service = service;
-	}
+	public restCountriesController(restCountriesService service){
+		this.service = service;}
 	@GetMapping("/{id}")
-	public restCountries getById (@PathVariable("id") Long id){
-		return service.getById(id);
+	public restCountriesService getById (@PathVariable("id") Long id){
+		return ((restCountriesController) service).getById(id);
 	}
 }
---
+++ restcountries-backend-api/src/main/java/co/simplon/p25/api/services/restCountriesService.java
@@ -0,0 +1,14 @@
+package co.simplon.p25.api.services;
+
+import java.util.List;
+
+
+import co.simplon.p25.api.entities.Country;
+
+public interface restCountriesService {
+	
+	List<Country> getCountries();
+}
+
+
+
